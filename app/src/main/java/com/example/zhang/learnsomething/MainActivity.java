fdab1050f97904a252db3912c4ec8cd7c74a1590
zgh <1078873743@qq.com>
2018-03-29 14:27:49 +0800

    增加测试

--- app/src/main/java/com/example/zhang/learnsomething/MainActivity.java
+++ app/src/main/java/com/example/zhang/learnsomething/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.zhang.learnsomething;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.vp)
+    ViewPager vp;
+    @InjectView(R.id.all)
+    RadioButton all;
+    @InjectView(R.id.android)
+    RadioButton android;
+    @InjectView(R.id.ios)
+    RadioButton ios;
+    @InjectView(R.id.fuli)
+    RadioButton fuli;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
     }
 }
