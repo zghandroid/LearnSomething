6aa68fa67b8325c7d63847a9113eb86b006ab5ab
zgh <1078873743@qq.com>
2018-07-09 11:34:22 +0800

    杂乱的学习

--- app/src/main/java/com/example/zhang/learnsomething/MainActivity.java
+++ app/src/main/java/com/example/zhang/learnsomething/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
